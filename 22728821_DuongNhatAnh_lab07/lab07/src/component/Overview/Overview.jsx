6dcdea0c29cb7e41007853ff27feece7758a04a1
duongnhutanh241805 <duongnhutanh1805@gmail.com>
2025-04-08 16:36:13 +0700

    cau 2 api overview

--- 22728821_DuongNhatAnh_lab07/lab07/src/component/Overview/Overview.jsx
+++ 22728821_DuongNhatAnh_lab07/lab07/src/component/Overview/Overview.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Overview.css";
 import li1 from "../../img/Squares four 1.png";
 import shop from "../../img/Button 1509.png";
 import dolar from "../../img/Button 1529.png";
 import person from "../../img/Button 1530.png";
+
 const Overview = () => {
+  const [overview, setOverview] = useState({
+    turnover: 0,
+    profit: 0,
+    newCustomer: 0,
+  });
+
+  useEffect(() => {
+    fetch("https://67f2bf95ec56ec1a36d4144f.mockapi.io/overview")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          setOverview(data[0]);
+        }
+      })
+      .catch((error) => console.error("Loi goi API:", error));
+  }, []);
+
   return (
     <div>
       <section>
@@ -23,25 +41,25 @@ const Overview = () => {
           <div className="card turnover">
             <div className="noidung">
               <h3>Turnover</h3>
-              <p>$92,405</p>
+              <p>${overview.turnover.toLocaleString()}</p>
             </div>
-            <img src={shop} />
+            <img src={shop} alt="Turnover" />
           </div>
 
           <div className="card profit">
             <div className="noidung">
               <h3>Profit</h3>
-              <p>$32,218</p>
+              <p>${overview.profit.toLocaleString()}</p>
             </div>
-            <img src={dolar} />
+            <img src={dolar} alt="Profit" />
           </div>
 
           <div className="card customers">
             <div className="noidung">
               <h3>New Customers</h3>
-              <p>298</p>
+              <p>${overview.newCustomer.toLocaleString()}</p>
             </div>
-            <img src={person} />
+            <img src={person} alt="New Customers" />
           </div>
         </div>
       </section>
